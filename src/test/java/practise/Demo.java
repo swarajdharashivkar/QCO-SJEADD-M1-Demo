4dd68d68e6edc64c6505bef0f006f97caaf20b95
aa <aa@aa-PC.bbrouter>
2023-06-13 20:19:00 +0530

    Demo class comitted

--- src/test/java/practise/Demo.java
+++ src/test/java/practise/Demo.java
@@ -2,15 +2,8 @@ package practise;
 
 public class Demo 
 {
-public static void main(String[] args) 
-{
-	System.out.println("hi");
-	System.out.println( "i commit again");
-	System.out.println("2 nd commit");
-	
-	
-	
-	
-	
-}
+  public static void main(String[] args) 
+  {
+	System.out.println("Hello");	
+  }
 }
